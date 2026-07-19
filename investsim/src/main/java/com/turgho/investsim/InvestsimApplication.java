b11851c9b75c8364b36765046500e8a8f60e135b
Turgho <victor.hugo3692111@gmail.com>
2026-07-19 13:08:19 -0300

    test: add maven wrapper and config

---
+++ investsim/src/main/java/com/turgho/investsim/InvestsimApplication.java
@@ -0,0 +1,13 @@
+package com.turgho.investsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestsimApplication.class, args);
+	}
+
+}
---
+++ investsim/src/test/java/com/turgho/investsim/InvestsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turgho.investsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
